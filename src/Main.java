3f9396d17be732d7adbf43ab98e224257a789176
Martin Gripenstedt <martingripenstedt@hotmail.com>
2023-10-26 13:36:28 +0200

    gitignore fix

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej Kalle!");
         System.out.println("HEj Martin!");
-        System.out.println("Hej då Kalle");
+        System.out.println("Hej då Kalle!");
     }
 }
